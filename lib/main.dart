e1ae512f82c294f1b3b9f6d0f15f2d733d7e5c2a
woravit <86755992+woravi@users.noreply.github.com>
2023-06-07 11:04:59 +0700

    3 taps pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,46 @@
-import 'package:flutter/foundation.dart';
+import 'package:calaulate/pages/calc.dart';
+import 'package:calaulate/pages/contact.dart';
+import 'package:calaulate/pages/home.dart';
 import 'package:flutter/material.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // const MyApp({ Key? key }) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: const Text("RW1968 Minimart")),
-          body: const Home(),
-        ));
+    return MaterialApp(home: MainPage());
   }
 }
-
-class Home extends StatefulWidget {
-  const Home({super.key});
+class MainPage extends StatefulWidget {
+  const MainPage({Key? key}) : super(key: key);
   @override
-  _HomeState createState() => _HomeState();
+  State<MainPage> createState() => _MainPageState();
 }
-
-class _HomeState extends State<Home> {
-  // ตำแหน่งสำหรับเก็บข้อมูลของค่าที่ user กรอกเข้ามา
-  TextEditingController quantity = TextEditingController();
-  TextEditingController price = TextEditingController(); // ช่องสำหรับเก็บจำนวน
-  // double price = 10; // ช่องสำหรับเก็บราคาสินค้า ใส่ค่าไปเลย
-  TextEditingController result = TextEditingController();
-
-  @override
-  void initState() {
-    // ฟังชั่นพิเศษที่ใช้สำหรับการตั้งค่าในตอนแรก
-    super.initState();
-
-    result.text = "ซื้อสินค้าจำนวน x ผล ราคา x บาท รวมลูกค้าต้องจ่ายทั้งหมด x บาท";
-
-  }
-
+class _MainPageState extends State<MainPage> {
+  int _currentIndex = 0;
+  final taps = [
+    HomePage(), CalculatePage(), ContactPage() //เก็บค่าหลายค่านับจาก 0
+  ];
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(50.0),
-          child: Center(
-              child: Column(
-                children: [
-                  Image.asset('assets/header_home.png', width: 300, ),
-                  const Text("RW Project", style: TextStyle(fontSize: 30,),),
-                  TextField(
-                      controller: price,
-                      decoration: const InputDecoration(
-                          labelText: 'ราคาสินค้า',
-                          border: OutlineInputBorder())),
-                  const SizedBox(height: 20),
-                  TextField(
-                      controller: quantity,
-                      decoration: const InputDecoration(
-                          labelText: 'จำนวน',
-                          border: OutlineInputBorder())),
-                  const SizedBox(height: 20),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      var cal = double.parse(quantity.text) * double.parse(price.text) ; // วิธีการแปลงข้อความเป็นตัวเลข: double.parse(quantity.text)
-                      if (kDebugMode) {
-                        print("Apple Qauntity: ${quantity.text} Total: $cal Baht");
-                      }
-
-                      setState(() {
-                        result.text = "ซื้อสินค้าจำนวน ${quantity.text} ชิ้น ราคาชิ้นละ ${price.text} บาท รวมลูกค้าต้องจ่ายทั้งหมด $cal บาท";
-                      });
-
-                    },
-                    style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(const Color(0xff60e00b)),
-                        padding: MaterialStateProperty.all(const EdgeInsets.fromLTRB(50, 20, 50, 20)),
-                        textStyle: MaterialStateProperty.all(const TextStyle(fontSize: 30))
-                    ),
-                    child: const Text("คำนวณ"),
-                  ),
-                  Text(result.text, style: const TextStyle(fontSize: 20),)
-                ],
-              )),
-        ),
-      ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("RW1968 Minimart")),
+      body: taps[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home),label: "หน้าแรก"),
+          BottomNavigationBarItem(icon: Icon(Icons.calculate),label: "คำนวณ"),
+          BottomNavigationBarItem(icon: Icon(Icons.contact_page),label: "ติดต่อเรา"),
+        ],
+        onTap: (index){
+          setState(() {
+            print(index);
+            _currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
-
-
